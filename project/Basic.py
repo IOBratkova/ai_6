efa1d94016d0b51ad7b61ff6c3e73e5199872e74
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-10 00:35:07 +0300

    Изменен файл Basic.py
    Добаавлены сущности Врач, Лор, Кардиолог, Волкова, Новиков. Волкова является врач. Новиков является врач. Волкова специальность кардиолог. Новиков специальность лор.

--- project/Basic.py
+++ project/Basic.py
@@ -1,6 +1,6 @@
 from Classes import Notion, Property, Linguistics, IsA
 
-reservedWords = ['Где', 'Кем']
+reservedWords = ['Где', 'Кем', 'Какая']
 
 entities = []  # Cущности
 relations = []  # Отношения
@@ -22,33 +22,59 @@ adress = Linguistics(entities[entities.index(Notion('Неболит'))],
                      entities[entities.index(Notion('ул. Князева'))])
 relations.append(adress)
 
-#Кем является Игорь?
+# Специальность - свойство врача
+entities.append(Notion('Врач'))
+entities.append(Notion('Волкова'))
+entities.append(Notion('Кардиолог'))
+entities.append(Notion('Новиков'))
+entities.append(Notion('Лор'))
+
+# Волкова является врач
+doctor1 = IsA(entities[entities.index(Notion('Волкова'))],
+              'является',
+              entities[entities.index(Notion('Врач'))])
+relations.append(doctor1)
+
+# Новиков является врач
+doctor2 = IsA(entities[entities.index(Notion('Новиков'))],
+              'является',
+              entities[entities.index(Notion('Врач'))])
+relations.append(doctor2)
+
+# Волкова специальность кардиолог
+kardio = Property(entities[entities.index(Notion('Волкова'))],
+                  'специальность',
+                  entities[entities.index(Notion('Кардиолог'))])
+propertis.append(kardio)
+
+# Новиков специальность лор
+lor = Property(entities[entities.index(Notion('Новиков'))],
+               'специальность',
+               entities[entities.index(Notion('Лор'))])
+propertis.append(lor)
+
+# Кем является Игорь? Ответ: Администратор
 entities.append(Notion('Игорь'))  # Сущность-понятие Игорь (Факт)
-entities.append(Notion('Администратор')) # Cущность-понятие администратор
+entities.append(Notion('Администратор'))  # Cущность-понятие администратор
 
 # Игорь является Администратором
 administrator = IsA(entities[entities.index(Notion('Игорь'))],
                     'является',
                     entities[entities.index(Notion('Администратор'))])
-
 relations.append(administrator)
 
-entities.append(Notion('пациент')) # Сущность-понятие пациент (Факт)
+entities.append(Notion('пациент'))  # Сущность-понятие пациент (Факт)
 
 # Кем является Марина? Ответ: пациент
-entities.append(Notion('Марина')) # Сущность-понятие Марина (Факт)
-
-# Кем является Костя? Ответ: пациент
-entities.append(Notion('Костя')) # Сущность-понятие Костя (Факт)
-
-# Марина является пациент
+entities.append(Notion('Марина'))  # Сущность-понятие Марина (Факт)
 patientMarina = IsA(entities[entities.index(Notion('Марина'))],
                     'является',
                     entities[entities.index(Notion('пациент'))])
-# Марина является пациент
+relations.append(patientMarina)
+
+# Кем является Костя? Ответ: пациент
+entities.append(Notion('Костя'))  # Сущность-понятие Костя (Факт)
 patientKostya = IsA(entities[entities.index(Notion('Костя'))],
                     'является',
                     entities[entities.index(Notion('пациент'))])
-
 relations.append(patientKostya)
-relations.append(patientMarina)
