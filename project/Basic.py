ebd8553fd9efc85416443c2f7eb17aeca0b2c990
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-10 13:39:27 +0300

    Basic.py++
    добавленые абстрактные сущности адрес, талон, специальность.
    Добавлена возможность завдавать вопросы про талоны.

--- project/Basic.py
+++ project/Basic.py
@@ -1,6 +1,6 @@
-from Classes import Notion, Property, Linguistics, IsA
+from Classes import Notion, Property, Linguistics, IsA, Event
 
-reservedWords = ['Где', 'Кем', 'Какая']
+reservedWords = ['Где', 'Кем', 'Какая', 'Какой']
 
 entities = []  # Cущности
 relations = []  # Отношения
@@ -9,10 +9,11 @@ propertis = []  # Свойства
 # Где находится Неболит?
 entities.append(Notion('Неболит'))  # Сущность-понятие больница "Неболит" факт
 entities.append(Notion('ул. Князева'))  # Сущность-понятие адрес "Неболит" факт
+entities.append(Notion('адрес'))
 
 # Свойство Адрес
 adress = Property(entities[entities.index(Notion('Неболит'))],
-                  'Адрес',
+                  entities[entities.index(Notion('адрес'))],
                   entities[entities.index(Notion('ул. Князева'))])
 propertis.append(adress)
 
@@ -41,15 +42,17 @@ doctor2 = IsA(entities[entities.index(Notion('Новиков'))],
               entities[entities.index(Notion('Врач'))])
 relations.append(doctor2)
 
+entities.append(Notion('специальность'))
+
 # Волкова специальность кардиолог
 kardio = Property(entities[entities.index(Notion('Волкова'))],
-                  'специальность',
+                  entities[entities.index(Notion('специальность'))],
                   entities[entities.index(Notion('Кардиолог'))])
 propertis.append(kardio)
 
 # Новиков специальность лор
 lor = Property(entities[entities.index(Notion('Новиков'))],
-               'специальность',
+               entities[entities.index(Notion('специальность'))],
                entities[entities.index(Notion('Лор'))])
 propertis.append(lor)
 
@@ -78,3 +81,23 @@ patientKostya = IsA(entities[entities.index(Notion('Костя'))],
                     'является',
                     entities[entities.index(Notion('пациент'))])
 relations.append(patientKostya)
+
+# талон 237
+entities.append(Notion('дата'))
+
+entities.append(Event('Талон237'))
+entities.append(Notion('02.01.2020'))
+date = Property(entities[entities.index(Event('Талон237'))],
+                entities[entities.index(Notion('дата'))],
+                entities[entities.index(Notion('02.01.2020'))])
+propertis.append(date)
+
+# талон 217
+entities.append(Event('Талон217'))
+entities.append(Notion('03.01.2020'))
+date2 = Property(entities[entities.index(Event('Талон217'))],
+                 entities[entities.index(Notion('дата'))],
+                 entities[entities.index(Notion('03.01.2020'))])
+propertis.append(date2)
+
+
